e02b116fdd37d66383665eeaaffa3375a692aeab
owner <toltolyibh@gmail.com>
2024-10-06 19:51:33 +0900

    SAGA 패턴 구현

---
+++ orders-service/src/main/java/com/appsdeveloperblog/orders/service/handler/OrderCommandsHandler.java
@@ -0,0 +1,24 @@
+package com.appsdeveloperblog.orders.service.handler;
+
+import com.appsdeveloperblog.core.dto.commands.ApprovedOrderCommand;
+import com.appsdeveloperblog.orders.service.OrderService;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(topics = "${orders.commands.topic.name}")
+public class OrderCommandsHandler {
+
+    private final OrderService orderService;
+
+    public OrderCommandsHandler(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @KafkaHandler
+    public void handleEvent(@Payload ApprovedOrderCommand approvedOrderCommand) {
+        orderService.approveOrder(approvedOrderCommand.getOrderId());
+    }
+}
